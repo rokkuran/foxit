d22be320ac9bb12580571a4739621eb72674edde
Unknown <lachlanbtaylor@gmail.com>
2018-07-24 19:53:33 +1000

    pre-merge

--- README.md
+++ README.md
@@ -1,25 +1,42 @@
-# Kitsu API Wrapper
-Ruby based wrapper for [Kitsu][kitsu] API ([documentation][api]). 
+# Kitsu API Wrapper and Recommenders
+Unofficial Ruby based wrapper for [Kitsu][kitsu] API ([documentation][api]). Various recommender implementations using data obtained from API.
 
 ## Current Functionality
 
-### User
-- Get user by id or name with associated attributes (minimal selection currently).
-- Get all user library entries with id, rating, timings.
-- Store data locally in mongodb - chunked batch requests implemented.
+### User Library
+Update details after code revision... 
+- Store data locally in mongodb.
 
-### Items
-- Get anime by id with associated attributes (majority available)
-- Store data locally in mongodb - chunked batch requests implemented.
+#### MongoDB Library Item Document
+Current fields obtained from Kitsu API. There are more available and could easilty be added.
+```
+{
+  '_id': ObjectId('5b56ad7dafd2a496c0faa495'),
+  'media_id': 1376,                                     # id for anime, e.g., this one related to 'Code Geass: Lelouch of the Rebellion'
+  'rating': 17,                                         # user rating [1-20]; None if not rated.
+  'record_id': 18344699,                                # library item id; used to return the actual media_id
+  'status': 'completed',                                # media viewing status: {completed, planned, watching, hold, dropped}
+  'type': 'anime',                                      # media type: {Anime, Manga, Drama?}.
+  'user_id': 1                                          # user id
+}
+```
+
+### Media Items
+- Get anime by id with associated attributes (majority available).
+- Store data locally in mongodb.
 
 
 ### Recommenders
 **Note**: python based recommendation code is broken due to re-write of API wrapper, which structures the database differently. I have left them as examples to be revised in the future.
 
 - LSA based recommendations using synopsis: tokenised; stemmed or not; tf-idf; SVD; cosine simliarity.
-- Preliminary work on content based recommender.
-- Preliminary work on SVD based collaborative filtering recommender.
+- Preliminary work on recommender examples using the [Surprise][surprise] library.
+
+- Old work on content based recommender.
+- Old work on SVD based collaborative filtering recommender.
+
 
 
 [kitsu]: kitsu.io
 [api]: https://kitsu.docs.apiary.io/
+[surprise]: http://surpriselib.com/
--- lsa.py
+++ lsa.py
@@ -1,110 +1,110 @@
-import numpy as np
-import pandas as pd
-
-from datetime import datetime
-import string
-
-import pymongo
-from pymongo import MongoClient
-
-from bs4 import BeautifulSoup
-from collections import Counter
-
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import SnowballStemmer
-from nltk.stem.porter import PorterStemmer
-
-from sklearn.pipeline import Pipeline, make_pipeline
-from sklearn.preprocessing import Normalizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import TruncatedSVD
-from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
-
-
-def get_data(cursor):
-    attributes = ['slug', 'synopsis']
-    results = []
-    for i, x in enumerate(cursor):
-        print(i, x['slug'], x['avg_rating'])
-        results.append([x[k] for k in attributes])
-
-    df = pd.DataFrame(results, columns=attributes)
-    return df
-
-
-def process_text(text, exclusions):
-    tokens = [s.lower() for s in word_tokenize(text)]
-    stemmer = PorterStemmer()
-    processed = [stemmer.stem(t) for t in tokens]
-    return tokens
-
-
-class StemmerTokenizer(object):
-    def __init__(self):
-        self.stemmer = SnowballStemmer('english')
-        # self.stemmer = PorterStemmer()
-
-    def mapper(self, word):
-        return self.stemmer.stem(word)
-
-    def tokenizer(self, doc):
-        # p = string.punctuation
-        # return [self.mapper(s) for s in word_tokenize(doc) if s not in p]
-        # return [s for s in word_tokenize(doc) if s not in p]
-        # return [x for x in word_tokenize(doc)]
-        return doc
-
-    def preprocessor(self, line):
-        return line.lower()
-
-
-if __name__ == '__main__':
-    client = MongoClient()
-    db = client.test
-    cursor = db.anime.find()
-
-    df = get_data(cursor)
-    y = df.slug.values
-    # X = asdf
-
-    tokenizer = StemmerTokenizer()
-    pipeline = make_pipeline(
-        TfidfVectorizer(
-            # preprocessor=tokenizer.preprocessor,
-            # tokenizer=tokenizer.tokenizer,
-            analyzer='word',
-            ngram_range=(1, 3),
-            min_df=0,
-            stop_words='english'
-        ),
-        TruncatedSVD(n_components=100),
-        Normalizer(copy=False)
-    )
-    X = pipeline.fit_transform(df.synopsis)
-    cs = cosine_similarity(X)
-
-    # using linear_kernel to calculate cosine similarity
-    tfidf_vector = TfidfVectorizer(
-        # preprocessor=tokenizer.preprocessor,
-        # tokenizer=tokenizer.tokenizer,
-        analyzer='word',
-        ngram_range=(1, 3),
-        min_df=0,
-        stop_words='english'
-    )
-    tfidf_matrix = tfidf_vector.fit_transform(df.synopsis)
-    cs_lk = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-    def recommend(index, cs, y, n=5):
-        a = np.argsort(cs, axis=1)[index][::-1][1:n + 1]
-        return zip(y[a], cs[index][a])
-
-    index = 1
-    print('\ncosine_similarity: %s' % y[index])
-    for k, v in recommend(index, cs, y, 10):
-        print('%s: %.4f' % (k, v))
-
-    print('\ncosine_similarity (linear_kernel): %s' % y[index])
-    for k, v in recommend(index, cs_lk, y, 10):
-        print('%s: %.4f' % (k, v))
+import numpy as np 
+import pandas as pd 
+ 
+from datetime import datetime 
+import string 
+ 
+import pymongo 
+from pymongo import MongoClient 
+ 
+from bs4 import BeautifulSoup 
+from collections import Counter 
+ 
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+from nltk.stem import SnowballStemmer 
+from nltk.stem.porter import PorterStemmer 
+ 
+from sklearn.pipeline import Pipeline, make_pipeline 
+from sklearn.preprocessing import Normalizer 
+from sklearn.feature_extraction.text import TfidfVectorizer 
+from sklearn.decomposition import TruncatedSVD 
+from sklearn.metrics.pairwise import cosine_similarity, linear_kernel 
+ 
+ 
+def get_data(cursor): 
+    attributes = ['slug', 'synopsis'] 
+    results = [] 
+    for i, x in enumerate(cursor): 
+        print(i, x['slug'], x['avg_rating']) 
+        results.append([x[k] for k in attributes]) 
+ 
+    df = pd.DataFrame(results, columns=attributes) 
+    return df 
+ 
+ 
+def process_text(text, exclusions): 
+    tokens = [s.lower() for s in word_tokenize(text)] 
+    stemmer = PorterStemmer() 
+    processed = [stemmer.stem(t) for t in tokens] 
+    return tokens 
+ 
+ 
+class StemmerTokenizer(object): 
+    def __init__(self): 
+        self.stemmer = SnowballStemmer('english') 
+        # self.stemmer = PorterStemmer() 
+ 
+    def mapper(self, word): 
+        return self.stemmer.stem(word) 
+ 
+    def tokenizer(self, doc): 
+        # p = string.punctuation 
+        # return [self.mapper(s) for s in word_tokenize(doc) if s not in p] 
+        # return [s for s in word_tokenize(doc) if s not in p] 
+        # return [x for x in word_tokenize(doc)] 
+        return doc 
+ 
+    def preprocessor(self, line): 
+        return line.lower() 
+ 
+ 
+if __name__ == '__main__': 
+    client = MongoClient() 
+    db = client.test 
+    cursor = db.anime.find() 
+ 
+    df = get_data(cursor) 
+    y = df.slug.values 
+    # X = asdf 
+ 
+    tokenizer = StemmerTokenizer() 
+    pipeline = make_pipeline( 
+        TfidfVectorizer( 
+            # preprocessor=tokenizer.preprocessor, 
+            # tokenizer=tokenizer.tokenizer, 
+            analyzer='word', 
+            ngram_range=(1, 3), 
+            min_df=0, 
+            stop_words='english' 
+        ), 
+        TruncatedSVD(n_components=100), 
+        Normalizer(copy=False) 
+    ) 
+    X = pipeline.fit_transform(df.synopsis) 
+    cs = cosine_similarity(X) 
+ 
+    # using linear_kernel to calculate cosine similarity 
+    tfidf_vector = TfidfVectorizer( 
+        # preprocessor=tokenizer.preprocessor, 
+        # tokenizer=tokenizer.tokenizer, 
+        analyzer='word', 
+        ngram_range=(1, 3), 
+        min_df=0, 
+        stop_words='english' 
+    ) 
+    tfidf_matrix = tfidf_vector.fit_transform(df.synopsis) 
+    cs_lk = linear_kernel(tfidf_matrix, tfidf_matrix) 
+ 
+    def recommend(index, cs, y, n=5): 
+        a = np.argsort(cs, axis=1)[index][::-1][1:n + 1] 
+        return zip(y[a], cs[index][a]) 
+ 
+    index = 1 
+    print('\ncosine_similarity: %s' % y[index]) 
+    for k, v in recommend(index, cs, y, 10): 
+        print('%s: %.4f' % (k, v)) 
+ 
+    print('\ncosine_similarity (linear_kernel): %s' % y[index]) 
+    for k, v in recommend(index, cs_lk, y, 10): 
+        print('%s: %.4f' % (k, v)) 
