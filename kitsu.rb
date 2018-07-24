9eb49c52f32d085fb81eb596a1e4355f44847bf9
Unknown <lachlanbtaylor@gmail.com>
2018-07-24 19:14:46 +1000

    large performance increase after incorporating threading

--- kitsu.rb
+++ kitsu.rb
@@ -1,7 +1,6 @@
-require 'oauth'
+require 'net/http'
 require 'json'
 require 'addressable/uri'
-
 
 
 class Helpers
@@ -13,179 +12,169 @@ class Helpers
 end
 
 
-class Configuration
-  attr_reader :root, :consumer
+class LibraryItem < Helpers
+  attr_reader :user_id, :record_id, :status, :rating, :media_id, :type
+
+  def initialize user_id, entry_result, media_result
+    @user_id = user_id.to_i
+    @record_id = entry_result['id'].to_i
+    @status = entry_result['attributes']['status']
+
+    rating = entry_result['attributes']['ratingTwenty']
+    @rating = rating.nil? ? nil : rating.to_i
+    
+    @media_id = media_result['data']['id'].to_i
+    @type = media_result['data']['type']
+  end
+end
+
+
+class Kitsu
 
   def initialize
-    # kitsu has not yet implemented application registration the following 
-    # key/secret pair are public and available on the API documentation site:
-    # https://kitsu.docs.apiary.io/#reference/authentication
-
-    key = "dd031b32d2f56c990b1425efe6c42ad847e7fe3ab46bf1299f05ecd856bdb7dd"
-    secret = "54d7307928f63414defd96399fc31ba847961ceaecef3a5fd93144e960c0e151"
     @root = "https://kitsu.io/api/edge/"
-    @consumer = OAuth::Consumer.new(key, secret)
-  end
-end
-
-
-class User < Helpers
-  attr_reader :id, :name, :about, :n_ratings
-
-  def initialize data
-    @id = data['id']
-    @name = data['attributes']['name']
-    @about = data['attributes']['about']
-    @n_ratings = data['attributes']['ratingsCount']
-  end
-  
-end
-
-
-class LibraryEntry < Helpers
-  attr_reader :id, :created, :updated, :started, :finished, :status, :rating
-
-  def initialize item
-    @id = item['id']
-    @created = item['attributes']['createdAt']
-    @updated = item['attributes']['updatedAt']
-    @started = item['attributes']['startedAt']
-    @finished = item['attributes']['finishedAt']
-    @status = item['attributes']['status']
-    @rating = item['attributes']['ratingTwenty']
   end
 
-end
 
-
-class Anime < Helpers
-  attr_reader :id, :slug, :synopsis, :title, :avg_rating, :rating_freq, :n_users,
-    :n_favourites, :start_date, :end_date, :rank_popularity, :rank_rating, :subtype,
-    :nsfw; :showtype
-
-  def initialize data
-    attributes = data['attributes']
-    @id = data['id']
-    @slug = attributes['slug']
-    @synopsis = attributes['synopsis']
-    @title = attributes['canonicalTitle']
-    @avg_rating = attributes['averageRating'].to_f  # TODO: handle nil values?
-    @rating_freq = attributes['ratingFrequencies']  # TODO: convert to {int: float} pairs
-    @n_users = attributes['userCount'].to_i
-    @n_favourites = attributes['favouritesCount'].to_i
-    @start_date = attributes['startDate']
-    @end_date = attributes['endDate']
-    @rank_popularity = attributes['popularityRank'].to_i
-    @rank_rating = attributes['ratingRank'].to_i
-    @subtype = attributes['subtype']
-    @showtype = attributes['showType']
-    @nsfw = attributes['nsfw']  # TODO: convert to bool?
-  end
-end
-
-
-class KitsuAPI < Configuration
-
-  def initialize
-    super()
-  end
-
-  def get_result url
-    response = @consumer.request(:get, url)
-    return JSON.parse(response.body)
-  end
-
-  def get_user_by_name name
-    # TODO: can return multiple users with same name...
-    uri = Addressable::URI.parse("#{@root}users")
-    uri.query_values = {"filter[name]" => name}
-    result = self.get_result(uri.to_s)
-    return User.new(result['data'][0])
-  end
-
-  def get_user_by_id id
-    uri = Addressable::URI.parse("#{@root}users/#{id}")
-    result = self.get_result(uri.to_s)
-    return result.key?('errors') ? nil : User.new(result['data'])
-  end
-
-  def get_user_library id, type, status, limit=500
-    """
-    id: user id
-    type: media type - Anime; Manga --> case sensitive
-    status: complete, watching, planned, hold, dropped
-    limit: number of results to return per page - max 500
-
-    returns: list of LibraryEntry objects
-    """
-
-    # initial parameters
+  def build_library_url id, type='Anime', status='completed', limit=500
     uri = Addressable::URI.parse("#{@root}library-entries")
 
     uri_query = {
       "filter[user_id]" => id,
-      "filter[media_type]" => type,  # 
+      "filter[media_type]" => type,
       "filter[status]" => status,
       "page[limit]" => limit
     }
-    
     uri.query_values = uri_query
-
-    result = self.get_result(uri.to_s)
-    entries = self._get_entries(result)
-
-    # get all results from subsquent pages as paginated response
-    while result['links'].key?('next')
-      result = self.get_result(result['links']['next'])
-      entries += self._get_entries(result)
-    end
-
-    return entries
+    
+    uri.to_s
   end
 
-  def _get_entries result
-    """
-    input: library-entries query result
-    return: list of LibraryEntry objects
-    """
-    entries = []
-    result['data'].each do |item|
-      entries << LibraryEntry.new(item)
-    end
-    return entries
+  
+  def build_media_url entry_id
+    "#{@root}/library-entries/#{entry_id}/relationships/media"
   end
 
-  def get_user_library_document id
-    user = self.get_user_by_id(id)
 
-    unless user.nil?
-      entries = self.get_user_library(id, 'Anime', 'completed', 500)
-    
-      doc = {
-        user: user.to_hash,
-        library: []
-      }
-    
-      entries.each do |x|
-        doc[:library] << x.to_hash
+  def get_result url
+    response = Net::HTTP.get(URI.parse(url))
+    JSON.parse(response)
+  end
+  
+
+  def get_library_by_url url, entries=[]
+
+    result = self.get_result(url)
+    entries += result['data']
+  
+    if result['links'].key?('next')
+      # recursion to retrieve additional results that have ben paginated
+      result = self.get_library_by_url(result['links']['next'], entries)
+    else
+      return entries
+    end
+  end
+
+
+  def get_library_by_id user_id
+    url = self.build_library_url(user_id)
+    self.get_library_by_url(url)
+  end
+
+
+  def get_media_by_id entry_id
+    url = self.build_media_url(entry_id)
+    self.get_result(url)
+  end
+
+
+  def batch_get_results ids, fn, max_threads
+    """
+    ids: ids of results returned: user_id | library_entry_id in this case.
+    fn: function name to use to return results (need to use symbol method name)
+        e.g. :get_result
+    max_threads: maximum number of active threads.
+    """
+
+    results = {}
+    threads = []
+
+    ids.each do |id|
+
+      if Thread.list.count % max_threads != 0
+        thread = Thread.new do
+          # adding lock slows down considerably shouldn't matter as results are written to hash?
+          results[id] = send(fn, id)
+        end
+        threads << thread
+      else
+        # wait for open threads to finish before starting new one
+        threads.each(&:join)
+
+        thread = Thread.new do
+          results[id] = send(fn, id)
+        end
+        threads << thread
       end
 
-      return doc
-    else
-      return nil
     end
+
+    threads.each(&:join)
+
+    results
   end
 
-  def get_anime_by_id id
-    uri = "#{@root}/anime/#{id}"
-    result = self.get_result(uri)
-    return result.key?('errors') ? nil : Anime.new(result['data'])
+
+  def get_batch_libraries user_ids, max_threads=200
+
+    all_library_entries = []
+    user_libraries = self.batch_get_results(user_ids, :get_library_by_id, max_threads)
+  
+    user_libraries.each do |user_id, library|
+  
+      media_ids = []
+      library.map { |entry| media_ids << entry['id'] }
+      media_results = self.batch_get_results(media_ids, :get_media_by_id, max_threads)
+      
+      library.each do |entry|
+        all_library_entries << LibraryItem.new(user_id, entry, media_results[entry['id']])
+      end
+  
+    end
+  
+    all_library_entries
+  end
+  
+
+  def get_batch_libraries_docs user_ids, max_threads=200
+    all_library_entries = self.get_batch_libraries(user_ids, max_threads)
+    
+    docs = []
+    all_library_entries.map { |entry| docs << entry.to_hash }
+
+    docs
   end
 
-  def get_anime_document id
-    result = self.get_anime_by_id(id)
-    return result.nil? ? nil : result.to_hash
+
+end
+
+
+
+def main
+
+  kitsu = Kitsu.new()
+  all_library_entries = kitsu.get_batch_libraries(1..10)
+
+  all_library_entries.each_with_index do |entry, i|
+    if i < 100
+      p "user_id=#{entry.user_id}; entry_id=#{entry.record_id}; media_id=#{entry.media_id}, rating=#{entry.rating}"
+    else
+      break
+    end
   end
 
 end
 
+
+
+# main()
