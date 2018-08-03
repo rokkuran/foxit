3063dd89ef96d0f1c81d546bcd549700b9a97b21
Unknown <lachlanbtaylor@gmail.com>
2018-08-03 13:37:42 +1000

    added some simple response tests

---
+++ tests/tc_get_anime_responses.rb
@@ -0,0 +1,44 @@
+require "test/unit"
+
+require_relative '../lib/foxit/api'
+
+
+
+class TestGetAnimeResponses < Test::Unit::TestCase
+
+  def setup
+    @api = Foxit::API.new()
+    @slug = 'cowboy-bebop'
+  end
+
+  def test__get_anime_by_attr_id_json
+    result = @api._get_anime_by_attr(:id, 1, :json)
+    assert_equal(@slug, result['data']['attributes']['slug'])
+  end
+
+  def test__get_anime_by_attr_id_object
+    result = @api._get_anime_by_attr(:id, 1, :object)
+    assert_equal(@slug, result.slug)
+  end
+
+  def test__get_anime_by_attr_slug_json
+    result = @api._get_anime_by_attr(:slug, @slug, :json)
+    assert_equal(@slug, result['data'][0]['attributes']['slug'])
+  end
+  
+  def test__get_anime_by_attr_slug_object
+    result = @api._get_anime_by_attr(:slug, @slug, :object)
+    assert_equal(@slug, result.slug)
+  end
+
+  def test_get_anime_by_id_base
+    result = @api.get_anime_by_id(1)
+    assert_equal(@slug, result.slug)
+  end
+
+  def test_get_anime_by_slug_base
+    result = @api.get_anime_by_slug(@slug)
+    assert_equal(@slug, result.slug)
+  end
+
+end
